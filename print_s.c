4c76b2947317e4336446459ee4622835e7325add
adam <aadamu7@yahoo.com>
2022-10-18 23:29:25 +0100

    printf

---
+++ print_s.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_s - print a string
+ * @s: string to print
+ *
+ * Return: i
+ */
+int print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+	int i;
+
+	if (!str)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	return (i);
+}
